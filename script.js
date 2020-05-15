f2f4693bc6b4571c0f89df8db121a459cf312785
Benjamin Brier <ben.brier14@gmail.com>
2020-05-14 22:53:54 -0400

    added event listener, app works

--- script.js
+++ script.js
@@ -46,3 +46,21 @@ function populateUI() {
 		movieSelect.selectedIndex = selectedMovieIndex;
 	}
 }
+// Movie select event
+movieSelect.addEventListener('change', (e) => {
+	ticketPrice = +e.target.value;
+	setMovieData(e.target.selectedIndex, e.target.value);
+	updateSelectedCount();
+});
+
+// Seat click event
+container.addEventListener('click', (e) => {
+	if (e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
+		e.target.classList.toggle('selected');
+
+		updateSelectedCount();
+	}
+});
+
+// Initial count and total set
+updateSelectedCount();
